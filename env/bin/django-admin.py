7263b26a7492fef49fd4921c6a528c7d0f30d8ed
Daryn <kapa.miki16@gmail.com>
2019-11-15 01:57:54 +0600

    remove env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/daryn/Documents/Daryn/Projects/Python/LittleProjects/pr_solution_task/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
